55aa600f18f75587fc1bfe273993f6ad8fd44600
Muhammad Andi Aziz <112955869+andimuhammadd@users.noreply.github.com>
2025-12-10 16:20:51 +0700

    Menambah Bagian Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react"
+import { Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./Pages/CreatePage"
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+  return (
+    <>
+    <button>Hello</button>
+    </>
+  )
+}
+
+export default App
