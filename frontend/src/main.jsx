aac9f4476aa18e2e56ab04795feef16dba2ef4d9
Muhammad Andi Aziz <112955869+andimuhammadd@users.noreply.github.com>
2025-12-17 15:39:06 +0700

    buat toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "@chakra-ui/react";
+import { toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ChakraProvider value={defaultSystem}>
         <App />
+        <Toaster toaster={toaster}>
+    {(toast) => (
+      <div>
+        <strong>{toast.title}</strong>
+        <div>{toast.description}</div>
+      </div>
+    )}
+  </Toaster>
       </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,48 +1,73 @@
-import { Container } from "lucide-react";
 import { useState } from "react";
-import { VStack, Heading, Box, Input, Button } from "@chakra-ui/react";
+import {
+  VStack,
+  Heading,
+  Box,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useProductStore } from "../store/product.js";
+import { toaster } from "@/components/ui/toaster";
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
     name: "",
-    price: 0,
-    image: ""
+    price: "",
+    image: "",
   });
 
-  const handleAddProduct = () => {
-    // Logic to add the new product
-    console.log("New Product:", newProduct);
-    // Reset form
-    setNewProduct({ name: "", price: "", image: "" });
-  }
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+
+    if (!success) {
+      toaster.create({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toaster.create({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+      setNewProduct({ name: "", price: "", image: "" });
+    }
+  };
 
   return (
-    <Box maxW="container.sm">
+    <Box maxW="container.sm" mx="auto" mt={10}>
       <VStack spacing={8}>
-        <Heading as="h1" size="2xl" textAlign="center" mb={8}>Create Product</Heading>
-        
-        <Box
-        w={"md"}
-        p={"6"} rounded={"lg"} shadow={"md"}
-        >
+        <Heading>Create Product</Heading>
+
+        <Box w="md" p={6} rounded="lg" shadow="md">
           <VStack spacing={4}>
-            <Input 
-            placeholder="Product Name"
-            name="name"
-            value={newProduct.name}
-            onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
+            <Input
+              placeholder="Product Name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
             />
-            <Input 
-            placeholder="Price"
-            name="price"
-            value={newProduct.price}
-            onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
+            <Input
+              placeholder="Price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
             />
-            <Input 
-            placeholder="Image URL"
-            name="image"
-            value={newProduct.image}
-            onChange={(e) => setNewProduct({...newProduct, image: e.target.value})}
+            <Input
+              placeholder="Image URL"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
             />
 
             <Button colorScheme="blue" w="full" onClick={handleAddProduct}>
@@ -52,7 +77,7 @@ const CreatePage = () => {
         </Box>
       </VStack>
     </Box>
-  )
-}
+  );
+};
 
-export default CreatePage
+export default CreatePage;
